8a3762314d7688542da139f27504ec2d8f1f0d58
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-04-21 13:04:20 +0100

    Add allowedHeaders property to corsOptions to fix CORS error

--- config/corsOptions.ts
+++ config/corsOptions.ts
@@ -1,16 +1,24 @@
-import { allowedOrigins } from "./allowedOrigins";
+import { allowedOrigins } from './allowedOrigins';
 
 export const corsOptions = {
   origin: (
     origin: string | undefined,
     callback: (err: Error | null, allow?: boolean) => void
   ) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (origin && allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     }
   },
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token'
+  ],
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
   credentials: true,
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200
 };
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import express from "express";
-import Router from "./routes";
-import cors from "cors";
-import { corsOptions } from "../config/corsOptions";
+import express from 'express';
+import Router from './routes';
+import cors from 'cors';
+import { corsOptions } from '../config/corsOptions';
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const port = 5000;
 
 const app = express();
+// These two lines give access to some origins while blocking others
+// The second line sets up a pre-flight request handler that responds to all
+// 'OPTIONS' requests with the appropriate CORS headers allowing the browser
+// to proceed with the actual request
 app.use(cors(corsOptions));
-app.use(express.json({ limit: "50mb" }));
+app.options('*', cors(corsOptions));
+
+app.use(express.json({ limit: '50mb' }));
 
 const start = async (): Promise<void> => {
   app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
-    app.use("/api/v1", Router);
+    app.use('/api/v1', Router);
   });
 };
 start();
