7f5e9880087f9dacc147709a701c65d0dde1bbce
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-04-24 18:09:44 +0100

    test(cors): allow all origin to identify the cause of CORS error

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,4 +1,5 @@
-export const allowedOrigins = [
-  'http://localhost:3000',
-  'https://growcally.onrender.com'
-];
+// export const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://growcally.onrender.com'
+// ];
+export const allowedOrigins = '*';
