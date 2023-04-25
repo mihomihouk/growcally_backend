b87a5459e633ffecf488c5086462ba12cb70f0be
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-04-25 10:11:03 +0100

    test(CORS): Move the cors related code in initializer function and got ditch the allowedHeader`

--- src/init/express.ts
+++ src/init/express.ts
@@ -1,10 +1,24 @@
 import express, { Application } from 'express';
-import { setupCors } from '../../config/cors';
+// import { setupCors } from '../../config/cors';
+import cors from 'cors';
 
 export const expressInitializer = (): Application => {
   const app = express();
-  setupCors(app);
+  // setupCors(app);
+
   app.use(express.json({ limit: '50mb' }));
+  const corsOptions = {
+    origin: ['http://localhost:3000', 'https://growcally.onrender.com'],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 200
+  };
+
+  // The second line sets up a pre-flight request handler that responds to all
+  // 'OPTIONS' requests with the appropriate CORS headers allowing the browser
+  // to proceed with the actual request
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
 
   return app;
 };
