19c98b7160eb0d7219361a2a74acad352d5ee4d7
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-05-26 11:28:04 +0100

    Fix: change reading root file config in production

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 
 const port = parseInt(process.env.PORT || '5000', 10);
 
-const start = async (): Promise<void> => {
+const start = async () => {
   const app = expressInitializer();
   app.use(express.json({ limit: '50mb' }));
   app.listen(port, () => {
