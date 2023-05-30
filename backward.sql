4160e2f49343de2a7b92964a9daedd0961281b15
Miho Inagaki <mihoinagaki@mihonomacbookea.home>
2023-05-30 17:33:09 +0100

    Fix: rollback migration to fix deploy error

---
+++ backward.sql
@@ -0,0 +1,3 @@
+yarn run v1.22.19
+$ '/Users/mihoinagaki/Documents/Job hunt/2023/growcally/growcally_backend/node_modules/.bin/prisma' migrate diff --from-url postgres://growcally:x7jHe6n2GR0R6CVJO9tnE1PZ7V43UCq7@dpg-chq6hdfdvk4goepevgag-a.frankfurt-postgres.render.com/growcally --to-migrations ./migrations --script
+info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
